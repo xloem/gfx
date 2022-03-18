509e9c8015647b2a5d70ef6c2d6412dddf7052be
Karl Semich <0xloem@gmail.com>
2022-03-18 10:22:08 -0400

    -

---
+++ py/gfx/sdl.py
@@ -0,0 +1,22 @@
+import sdl2
+import sdl2.ext as lib
+
+from . import ascii
+
+lib.init()
+
+# stub draft
+
+class SDL2:
+    def __init__(self, size=None):
+        if size is None:
+            size = ascii.ASCII().dims
+        self.window = lib.Window('', size=size)
+        self.window.show()
+        self.renderer = lib.Renderer(self.window)
+    def plot(self, x, y, color=(1.0,1.0,1.0)):
+        self.renderer.draw_point([x,y], lib.Color(*(int(color * 255 + 0.5) for color in color)))
+    def blit(self):
+        self.renderer.present()
+
+
