065dadcccf9013b274ca9c52b05e3a3b3e094a2e
Karl Semich <0xloem@gmail.com>
2022-03-18 16:22:47 -0400

    -

--- py/gfx/sdl.py
+++ py/gfx/sdl.py
@@ -1,7 +1,7 @@
 import sdl2
 import sdl2.ext as lib
 
-from . import ascii
+import ascii
 
 lib.init()
 
@@ -14,9 +14,14 @@ class SDL2:
         self.window = lib.Window('', size=size)
         self.window.show()
         self.renderer = lib.Renderer(self.window)
-    def plot(self, x, y, color=(1.0,1.0,1.0)):
-        self.renderer.draw_point([x,y], lib.Color(*(int(color * 255 + 0.5) for color in color)))
+    def plot(self, vec2, color=(1.0,1.0,1.0)):
+        self.renderer.draw_point(vec2, lib.Color(*(int(color * 255 + 0.5) for color in color)))
     def blit(self):
         self.renderer.present()
 
 
+
+if __name__ == '__main__':
+    sdl2 = SDL2()
+    #for x in range(
+    print(sdl2)
